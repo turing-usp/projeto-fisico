fa1225f9c16b3560027d51e36c301a9cf24e7dfb
Fernando Matsumoto <ferkmatsumoto@gmail.com>
2021-03-19 09:23:45 -0300

    Melhora __repr__ dos schedulers

--- src/schedulers.py
+++ src/schedulers.py
@@ -23,6 +23,18 @@ class Scheduler(ABC):
     def value(self):
         return self.__f_value
 
+    def __make_repr(self, *args, **kwargs):
+        arg_strs = [
+            *(repr(v) for v in args),
+            *(k + '=' + repr(v) for k, v in kwargs.items()),
+        ]
+        return self.__class__.__name__ + '(' + ', '.join(arg_strs) + ')'
+
+    def __repr__(self, *args, **kwargs):
+        if self.__f != identity:
+            kwargs.setdefault('f', self.__f)
+        return self.__make_repr(*args, **kwargs)
+
     @abstractmethod
     def step(self):
         pass
@@ -33,10 +45,13 @@ class PiecewiseScheduler(Scheduler):
         super().__init__(**kwargs)
         assert parts[0][0] == 0
         parts.sort()
+        self.parts = parts.copy()
         self.steps_remaining = parts
 
         self.n = -1
         self.step()
+
+        self._repr = f'PiecewiseScheduler(' + repr(self.parts) + ')'
 
     def step(self):
         if not self.steps_remaining:
@@ -47,6 +62,9 @@ class PiecewiseScheduler(Scheduler):
         if self.n == target_n:
             self.update(new_val)
             self.steps_remaining.pop(0)
+
+    def __repr__(self):
+        return super().__repr__(self.parts)
 
 
 class LinearScheduler(Scheduler):
@@ -64,21 +82,35 @@ class LinearScheduler(Scheduler):
             return
 
         self.n += 1
-        val = ((self.num_episodes-self.n)*self.start + self.n*self.end) / self.num_episodes
+        val = ((self.num_episodes-self.n)*self.start +
+               self.n*self.end) / self.num_episodes
         self.update(val)
+
+    def __repr__(self):
+        return super().__repr__(self.start, self.end, self.num_episodes)
 
 
 class ExponentialScheduler(Scheduler):
     def __init__(self, initial_value, multiplier, min_value=None, max_value=None, **kwargs):
         super().__init__(**kwargs)
         self._value = initial_value
+        self.initial_value = initial_value
         self.multiplier = multiplier
         self.min_value = min_value
         self.max_value = max_value
 
     def step(self):
-        self._value = np.clip(self._value * self.multiplier, self.min_value, self.max_value)
+        self._value = np.clip(self._value * self.multiplier,
+                              self.min_value, self.max_value)
         self.update(self._value)
+
+    def __repr__(self):
+        kwargs = {}
+        if self.min_value is not None:
+            kwargs['min_value'] = self.min_value
+        if self.max_value is not None:
+            kwargs['max_value'] = self.max_value
+        return super().__repr__(self.initial_value, self.multiplier, **kwargs)
 
 
 def find_schedulers(obj, base='', d=None):
